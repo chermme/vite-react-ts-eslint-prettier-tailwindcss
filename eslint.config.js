4c7c2ff453a97dea0870c440413aa96f2316c377
Peter Chermanowicz <pcherm@icloud.com>
2025-02-13 12:39:11 +0000

    Basic template setup

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettierEslint from 'eslint-config-prettier'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { 'react-hooks': reactHooks, 'react-refresh': reactRefresh },
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettierEslint,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+]
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css'
+
+function App() {
+  return <div>My app goes here</div>
+}
+
+export default App
